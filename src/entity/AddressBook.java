a4ac6789b780823d08862fa3d6e8818acdd26a46
sneh3195 <snehjainnsk@gmail.com>
2021-11-12 14:59:03 +0530

    USeCase-2:add new contact to address book

--- src/entity/AddressBook.java
+++ src/entity/AddressBook.java
@@ -3,14 +3,14 @@ import java.util.ArrayList;
 public class AddressBook {
 	
 	
-	public static ArrayList<ContactPerson> contacts = new ArrayList<ContactPerson>();
+	public static ArrayList<ContactPerson> contact = new ArrayList<ContactPerson>();
 
 	public static ArrayList<ContactPerson> getContacts() {
-		return contacts;
+		return contact;
 	}
 
 	public static void setContacts(ArrayList<ContactPerson> contacts) {
-		AddressBook.contacts = contacts;
+		AddressBook.contact = contacts;
 	}
 	
 	
